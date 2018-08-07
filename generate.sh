e3471f78e0e5754be8e6d41289fc1306fc7b8eeb
climbah <daenu.schmid@gmail.com>
2018-08-07 18:58:14 +0200

    added pdf to ignore and changed generate

--- generate.sh
+++ generate.sh
@@ -1,3 +1,15 @@
-pdflatex --output-directory=.includes/out/letter/ .includes/templates/letter_template.tex
+##############################
+## SET VARIABLES
+##############################
+lettertemplate="letter_template"
 
-pdflatex --output-directory=.includes/out/cv/ .includes/templates/cv_template.tex
+##############################
+## GENERATE THE TEMPLATES
+##############################
+pdflatex --output-directory='includes/out/letter/' 'includes/templates/'"$lettertemplate"'.tex'
+pdflatex --output-directory='includes/out/cv/' 'includes/templates/cv_template.tex'
+##############################
+## COPY OUTPUT TO OUT
+##############################
+cp 'includes/out/letter/'"$lettertemplate"'.pdf' ./out/motivationsschreiben.pdf
+cp 'includes/out/cv/cv_template.pdf' .
