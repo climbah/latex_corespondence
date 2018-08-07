c55164d5af7ff61d943c22505cfc2d2d615cf0a8
climbah <daenu.schmid@gmail.com>
2018-08-07 17:31:40 +0200

    modified files

--- generate.sh
+++ generate.sh
@@ -1,3 +1,23 @@
-pdflatex --output-directory=.includes/out/letter/ .includes/templates/letter_template.tex
+letter="letter_template"
+##############################
+# BACKUPS
+##############################
 
-pdflatex --output-directory=.includes/out/cv/ .includes/templates/cv_template.tex
+##############################
+# Generate INCLUDES
+##############################
+pdflatex --output-directory=includes/out/letter "includes/templates/${letter}.tex"
+pdflatex --output-directory=includes/out/cv "includes/templates/cv_template.tex"
+
+##############################
+# COPY
+##############################
+cp "includes/out/letter/${letter}.pdf" "out/motivationsschreiben.pdf"
+cp includes/out/cv/cv.pdf out/cv.pdf
+
+
+
+##############################
+# OPEN LETTER
+#############################
+#evince out/motivationsschreiben.pdf
