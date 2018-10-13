b7c90ea701047b596e10d12fb966c96586d7303f
climbah <daenu.schmid@gmail.com>
2018-10-14 00:50:36 +0200

    changes

--- generate.sh
+++ generate.sh
@@ -1,31 +1,34 @@
 ##############################
 ## SET GENERAL VARIABLES
 ##############################
-lettertemplate="letter_template"
+#lettertemplate="letter_template"
+lettertemplate="LetterTemplate"
 
 ##############################
-## CREATING DIRECTORIES
+## CREATING OUT DIRECTORIES
 ##############################
 mkdir -p out/arbeitszeugnisse
 mkdir out/zertifikate
 mkdir -p includes/out/letter
 mkdir includes/out/cv
-##############################
-## CHECKING FOR PARAMETERS
-##############################
-if [[ $1 != "" ]]
-then
-    lettertemplate=$1
-    echo $lettertemplate
-fi
 
 ##############################
 ## GENERATE THE TEMPLATES
 ##############################
 pdflatex --output-directory='includes/out/letter/' 'includes/templates/letter/'"$lettertemplate"'.tex'
 pdflatex --output-directory='includes/out/cv/' 'includes/templates/cv/template.tex'
+
 ##############################
 ## COPY OUTPUT TO OUT
 ##############################
 cp 'includes/out/letter/'"$lettertemplate"'.pdf' ./out/motivationsschreiben.pdf
 cp 'includes/out/cv/cv_template.pdf' .
+
+##############################
+## CHECKING FOR PARAMETERS
+##############################
+if [[ $1 == "show" ]]
+then
+    evince "./out/motivationsschreiben.pdf"
+fi
+
