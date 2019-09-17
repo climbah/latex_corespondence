caec5990195b0356b22a41b7b507e905dfd54ee9
climbah <daenu.schmid@gmail.com>
2019-09-17 17:12:18 +0200

    getting work again

--- generate.sh
+++ generate.sh
@@ -2,16 +2,18 @@
 ## SET GENERAL VARIABLES
 ##############################
 #lettertemplate="letter_template"
-home=~/GITLAB/latex_dossier
+home=$PWD
 lettertemplate="business2"
 cvtemplate="template"
 
 ##############################
 ## Cleanup and Copy 
 ##############################
+rm out -rf
 cd includes
 ./CopyJob.sh
 cd $home
+mkdir out
 ##############################
 ## GENERATE THE TEMPLATES
 ##############################
@@ -22,21 +24,37 @@ xelatex --output-directory='out/' "$cvtemplate"'.tex'
 ##############################
 ## COPY OUTPUT TO OUT
 ##############################
-cp 'out/'"$lettertemplate"'.pdf' ../../out/motivation.pdf
-cp 'out/'"$cvtemplate"'.pdf' ../../out/cv.pdf
+cp 'out/'"$lettertemplate"'.pdf' $home/out/motivation.pdf -f
+cp 'out/'"$cvtemplate"'.pdf' $home/out/cv.pdf -f
 cd $home
-
+pdftk ./out/motivation.pdf ./out/cv.pdf cat output ./out/full.pdf
 ##############################
 ## MAKING ZIP
 ##############################
 cd out
-pdfsam -f motivationsschreiben.pdf -f cv.pdf -o candidature.pdf 
+#pdfsam -f motivationsschreiben.pdf -f cv.pdf -o candidature.pdf 
 ##############################
 ## CHECKING FOR PARAMETERS
 ##############################
 if [[ $1 == "show" ]]
 then
 	cd $home
-    evince "./out/cv.pdf"
+    evince "./out/full.pdf"
+else
+    echo -n "Open Output?: "
+    read inp
+    if [[ $inp == "y" ]]
+    then
+        cd $home
+        evince "./out/cv.pdf"
+    fi
+fi
+
+echo -n "Finalize?: "
+read inp
+if [[ $inp == "y" ]]
+then
+echo 'finalize'
+echo 'create zip'
 fi
 
