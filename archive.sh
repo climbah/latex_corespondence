d9cbc0c4e4cfcf805979ecfddeb266b7760fd0da
climbah <daenu.schmid@gmail.com>
2020-04-24 13:31:43 +0200

    change archive

--- archive.sh
+++ archive.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-home=.
-cd conf
+cd Archiv
 ls -lah | grep a
 
 #evince ./out/motivationsschreiben.pdf
 
 read -p 'Archive Name: ' name
+cd ../conf/
 7z a $name.7z *.tex
 mv $name.7z ./../Archiv/
 echo ''"$name"'.7z'
